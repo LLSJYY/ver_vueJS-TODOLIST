8937f7f58f42814a2a48d901738072569b758113
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2022-09-22 03:43:06 +0900

    왜 빈 li가 하나 만들어지나요

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 <script setup>
   import { ref } from 'vue';
   const todos = ref([{}]);
+  
   function TodoInput(event){
     todos.value.push( {
       title:event.target.value,
@@ -14,7 +15,9 @@
 
 <template>
   <input @keyup.enter="TodoInput"  placeholder="What needs to be done?">
-
+  <ul>
+    <li v-for="item of todos" :key="item.id">{{item.title}}</li>
+  </ul>
 </template>
 
 <style>
