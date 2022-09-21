88b6593e9ce72789f46304f823b3cdd7ad999aba
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2022-09-22 03:34:38 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+  import { ref } from 'vue';
+  const todos = ref([{}]);
+  function TodoInput(event){
+    todos.value.push( {
+      title:event.target.value,
+      id: Math.floor(Math.random()*5000),
+      completed:false,
+    });
+   console.log(todos); 
+  }
+</script>
+
+<template>
+  <input @keyup.enter="TodoInput"  placeholder="What needs to be done?">
+
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
